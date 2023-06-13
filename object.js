4b09e732ca32fbe1a28f054af0cecc7ca2359017
Nsaravanan89 <136297971+Nsaravanan89@users.noreply.github.com>
2023-06-13 13:44:04 +0530

    task file

---
+++ object.js
@@ -0,0 +1,13 @@
+var a = [{ name: "saro" }
+    , { regno:"101" }
+    , { bloodgroup:"B+" }
+    , { dept:"computer" }
+    , { rollno:"43" }
+    , { place:"musiri" }
+    , { college:"gpt" }
+    , { year:"second year" }
+    , { shift:"first shift" }
+    , { age:"18" }]
+console.log(a);
+
+
